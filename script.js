a03b6763381c65396705ce296628e69817bee3d5
Ortiz Cota <josephortizcota@my.whccd.edu>
2020-01-13 20:31:39 -0800

    Learned how to use the debugger in visual studio code.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello from my External Script!");
+console.log("Hello from my first log statement");
+
+console.log("Hello from my second log statement");
+
+console.log("Hello from my third log statement");
